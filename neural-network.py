115377a34011bc8fff3c1e68647cfc7419811364
Dominik <101473446+b14ucky@users.noreply.github.com>
2024-05-02 17:20:27 +0200

    feat: improve the neural network (#2)
    * refactor: rewrite neuralnetwork class so it uses layer class for better customization
    
    * feat: add softmax activation and cross-entropy loss
    
    * feat: implement mini batch gradient descent
    
    * refactor: replace np.ndarray typing with np.typing.NDArray
    
    * feat: add loss and accuracy plotting
    
    * fix: clear plot results before displaying numbers
    
    * feat: add adam optimizer and learning rate decay
    
    * feat: add image augmentation to increase performance
    
    * chore: update readme

--- neural-network.py
+++ neural-network.py
@@ -1,76 +1,329 @@
 import numpy as np
+from numpy.typing import NDArray
 import matplotlib.pyplot as plt
-from utils import DataLoader, EpochAction, LearningRateAction, DrawingApp
+from utils import (
+    DataLoader,
+    NonpositiveIntAction,
+    LearningRateAction,
+    DrawingApp,
+    ImageAugmentation,
+    plot,
+)
 import argparse
 
 
-class NeuralNetwork:
-    def __init__(self, input_nodes, hidden_nodes, output_nodes):
-        self.w_input_hidden = np.random.uniform(-0.5, 0.5, (hidden_nodes, input_nodes))
-        self.w_hidden_output = np.random.uniform(-0.5, 0.5, (output_nodes, hidden_nodes))
-        self.b_input_hidden = np.zeros((hidden_nodes, 1))
-        self.b_hidden_output = np.zeros((output_nodes, 1))
+class Layer:
+    def __init__(self, input_size: int, output_size: int, activation: str = "sigmoid"):
+        """
+        Args:
+            input_size (int): The number of input nodes
+            output_size (int): The number of output nodes
+            activation (str, optional): The activation function to use. Defaults to "sigmoid".
+        """
+        # initialize the weights using he-initialization
+        self.weights: NDArray = np.random.randn(input_size, output_size) * np.sqrt(2 / input_size)
+        self.biases: NDArray = np.zeros((1, output_size))
+        self.activation: str = activation
 
-    def _sigmoid(self, x):
+        # adam optimizer parameters
+        self.m_weights: NDArray = np.zeros_like(self.weights)
+        self.v_weights: NDArray = np.zeros_like(self.weights)
+        self.m_biases: NDArray = np.zeros_like(self.biases)
+        self.v_biases: NDArray = np.zeros_like(self.biases)
+        self.beta1: float = 0.9
+        self.beta2: float = 0.999
+        self.epsilon: float = 1e-8
+
+    def __call__(self, input: NDArray) -> NDArray:
+        """
+        Overload the call operator to make the layer callable
+        Args:
+            input (NDArray): The input to the layer
+        Returns:
+            NDArray: The output of the layer
+        """
+        return self._forward(input)
+
+    def _forward(self, input: NDArray) -> NDArray:
+        """
+        Forward pass through the layer
+        Args:
+            input (NDArray): The input to the layer
+        Returns:
+            NDArray: The output of the layer
+        """
+        self.input: NDArray = input
+
+        pre_activation_output: NDArray = self.input @ self.weights + self.biases
+
+        # Apply activation functions
+        if self.activation == "sigmoid":
+            self.output: NDArray = self._sigmoid(pre_activation_output)
+        elif self.activation == "relu":
+            self.output: NDArray = self._relu(pre_activation_output)
+        elif self.activation == "softmax":
+            self.output: NDArray = self._softmax(pre_activation_output)
+
+        else:
+            print(f"The activation function entered does not exist!\nUse either relu or softmax")
+
+        return self.output
+
+    def backward(self, gradient: NDArray, learning_rate: float, t: int) -> NDArray:
+        """
+        Backward pass through the layer
+        Args:
+            gradient (NDArray): The gradient of the loss with respect to the output of the layer
+            learning_rate (float): The learning rate to use for updating the weights and bias
+            t (int): The current iteration for the Adam optimizer
+        Returns:
+            NDArray: The gradient of the loss with respect to the input of the layer
+        """
+        # calculate the delta l
+        if self.activation == "sigmoid":
+            delta: NDArray = gradient * self._dsigmoid(self.output)
+        elif self.activation == "relu":
+            delta: NDArray = gradient * self._drelu(self.output)
+        elif self.activation == "softmax":
+            delta: NDArray = self._dsoftmax(self.output, gradient)
+
+        # calculate the gradients with respect to the weights and bias
+        weights_gradient: NDArray = self.input.T @ delta
+        biases_gradient: NDArray = np.sum(delta, axis=0, keepdims=True)
+
+        # clip the gradients to prevent exploding gradients
+        weights_gradient = np.clip(weights_gradient, -1.0, 1.0)
+        biases_gradient = np.clip(biases_gradient, -1.0, 1.0)
+
+        # update the weights and bias
+        self.weights -= learning_rate * weights_gradient
+        self.biases -= learning_rate * biases_gradient
+
+        # calculate the moving average of the gradients at time step t
+        self.m_weights = self.beta1 * self.m_weights + (1 - self.beta1) * weights_gradient
+        self.v_weights = self.beta2 * self.v_weights + (1 - self.beta2) * weights_gradient**2
+        self.m_biases = self.beta1 * self.m_biases + (1 - self.beta1) * biases_gradient
+        self.v_biases = self.beta2 * self.v_biases + (1 - self.beta2) * biases_gradient**2
+
+        # calculate the corrected moving averages
+        corrected_m_weights = self.m_weights / (1 - self.beta1**t)
+        corrected_v_weights = self.v_weights / (1 - self.beta2**t)
+        corrected_m_biases = self.m_biases / (1 - self.beta1**t)
+        corrected_v_biases = self.v_biases / (1 - self.beta2**t)
+
+        # update the weights and bias using the Adam optimizer
+        self.weights -= (
+            learning_rate * corrected_m_weights / (np.sqrt(corrected_v_weights) + self.epsilon)
+        )
+        self.biases -= (
+            learning_rate * corrected_m_biases / (np.sqrt(corrected_v_biases) + self.epsilon)
+        )
+
+        return delta @ self.weights.T
+
+    def _sigmoid(self, x: NDArray) -> NDArray:
+        """
+        The sigmoid activation function
+        Args:
+            x (NDArray): The input to the activation function
+        Returns:
+            NDArray: The output of the activation function
+        """
         return 1 / (1 + np.exp(-x))
+
+    def _dsigmoid(self, x: NDArray) -> NDArray:
+        """
+        The derivative of the sigmoid activation function
+        Args:
+            x (NDArray): The input to the activation function
+        Returns:
+            NDArray: The output of the derivative of the activation function
+        """
+        return self._sigmoid(x) * (1 - self._sigmoid(x))
+
+    def _relu(self, x: NDArray) -> NDArray:
+        """
+        The ReLU activation function
+        Args:
+            x (NDArray): The input to the activation function
+        Returns:
+            NDArray: The output of the activation function
+        """
+        return np.maximum(0, x)
+
+    def _drelu(self, x: NDArray) -> NDArray:
+        """
+        The derivative of the ReLU activation function
+        Args:
+            x (NDArray): The input to the activation function
+        Returns:
+            NDArray: The output of the derivative of the activation function
+        """
+        return x > 0
+
+    def _softmax(self, x: NDArray) -> NDArray:
+        """
+        The softmax activation function
+        Args:
+            x (NDArray): The input to the activation function
+        Returns:
+            NDArray: The output of the activation function
+        """
+        exponential_values: NDArray = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return exponential_values / np.sum(exponential_values, axis=-1, keepdims=True)
+
+    def _dsoftmax(self, x: NDArray, gradient: NDArray) -> NDArray:
+        """
+        The derivative of the softmax activation function
+        Args:
+            x (NDArray): The input to the activation function
+            gradient (NDArray): The gradient of the loss with respect to the output of the layer
+        Returns:
+            NDArray: The output of the derivative of the activation function
+        """
+        for i, d_value in enumerate(gradient):
+            if len(d_value.shape) == 1:
+                d_value = d_value.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(d_value) - (d_value @ d_value.T)
+            gradient[i] = jacobian_matrix @ self.output[i]
+
+        return gradient
+
+
+class NeuralNetwork:
+    def __init__(self, input_nodes: int, hidden_nodes: list[int], output_nodes: int) -> None:
+        """
+        Args:
+            input_nodes (int): The number of input nodes
+            hidden_nodes (list[int]): The number of hidden nodes
+            output_nodes (int): The number of output nodes
+        """
+        self.l1 = Layer(input_nodes, hidden_nodes[0], "relu")
+        self.l2 = Layer(hidden_nodes[0], hidden_nodes[1], "relu")
+        self.l3 = Layer(hidden_nodes[1], output_nodes, "softmax")
+
+    def __call__(self, input: NDArray) -> NDArray:
+        """
+        Overload the call operator to make the neural network callable
+        Args:
+            input (NDArray): The input to the neural network
+        Returns:
+            NDArray: The output of the neural network
+        """
+        return self._forward(input)
+
+    def _forward(self, input: NDArray) -> NDArray:
+        """
+        Forward pass through the neural network
+        Args:
+            input (NDArray): The input to the neural network
+        Returns:
+            NDArray: The output of the neural network
+        """
+        output1 = self.l1(input)
+        output2 = self.l2(output1)
+        output = self.l3(output2)
+
+        return output
 
     def train(
         self,
-        learning_rate,
-        epochs,
-        images,
-        labels,
-        learning_rate_decay=1,
-        lr_decay_epoch=1,
-    ):
-        num_correct = 0
+        learning_rate: float,
+        epochs: int,
+        batch_size: int,
+        train_images: NDArray,
+        train_labels: NDArray,
+        test_images: NDArray,
+        test_labels: NDArray,
+        plot_stats: bool = False,
+        learning_rate_decay: float = 0.001,
+    ) -> None:
+        """
+        Train the neural network
+        Args:
+            learning_rate (float): The learning rate to use for training
+            epochs (int): The number of epochs to train the neural network
+            batch_size (int): The batch size to use for training
+            images (NDArray): The images to train on
+            labels (NDArray): The labels of the images
+            learning_rate_decay (float, optional): The learning rate decay factor. Defaults to 1.
+        """
+        loss_plot = []
+        train_accuracy_plot = []
+        test_accuracy_plot = []
+        t = 0
         for epoch in range(epochs):
-            for image, label in zip(images, labels):
-                image.shape += (1,)
-                label.shape += (1,)
+            for i in range(0, len(train_images), batch_size):
+                batch_images = train_images[i : i + batch_size]
+                batch_labels = train_labels[i : i + batch_size]
 
-                # Forward propagation (input to hidden)
-                hidden_pre = self.b_input_hidden + self.w_input_hidden @ image
-                hidden = self._sigmoid(hidden_pre)
+                for i, image in enumerate(batch_images):
+                    image = ImageAugmentation(image)()
+                    batch_images[i] = image
 
-                # Forward propagation (hidden to output)
-                output_pre = self.b_hidden_output + self.w_hidden_output @ hidden
-                output = self._sigmoid(output_pre)
+                output: NDArray = self._forward(batch_images)
 
-                # Calculate error
-                error = 1 / len(output) * np.sum((output - label) ** 2, axis=0)
-                num_correct += int(np.argmax(output) == np.argmax(label))
+                # calculate the gradient of the loss with respect to the output
+                delta_output = (output - batch_labels) / output.shape[0]
 
-                # Backpropagation (output to hidden)
-                delta_output = output - label
-                self.w_hidden_output += -learning_rate * delta_output @ np.transpose(hidden)
-                self.b_hidden_output += -learning_rate * delta_output
+                # backpropagate the gradient
+                t += 1
+                delta_hidden2 = self.l3.backward(delta_output, learning_rate, t)
+                delta_hidden1 = self.l2.backward(delta_hidden2, learning_rate, t)
+                delta_input = self.l1.backward(delta_hidden1, learning_rate, t)
 
-                # Backpropagation (hidden to input)
-                delta_hidden = (
-                    np.transpose(self.w_hidden_output) @ delta_output * (hidden * (1 - hidden))
-                )
-                self.w_input_hidden += -learning_rate * delta_hidden @ np.transpose(image)
-                self.b_input_hidden += -learning_rate * delta_hidden
+            # decay the learning rate
+            learning_rate /= 1 + learning_rate_decay
 
-            print(f"Epoch {epoch + 1}: {num_correct / len(images) * 100:.2f}%")
-            num_correct = 0
+            # calculate the loss using the cross-entropy loss function
+            epsilon = 1e-10
+            error: float = -np.mean(batch_labels * np.log(output + epsilon))
+            loss_plot.append(error)
 
-            print(f"Learning rate: {learning_rate}")
-            if epoch % lr_decay_epoch == 0 and epoch != 0:
-                learning_rate *= learning_rate_decay
+            # calculate the accuracy
+            predicted_labels: NDArray = np.argmax(output, axis=1)
+            true_labels: NDArray = np.argmax(batch_labels, axis=1)
+            train_accuracy: float = np.mean(predicted_labels == true_labels) * 100
+            train_accuracy_plot.append(train_accuracy)
 
-    def predict(self, image):
-        image.shape += (1,)
+            print(
+                f"Epoch {epoch + 1} - Train accuracy: {train_accuracy:.2f}% - Loss: {error:.4f}",
+                end=" - ",
+            )
 
-        # Forward propagation (input to hidden)
-        hidden_pre = self.b_input_hidden + self.w_input_hidden @ image
-        hidden = self._sigmoid(hidden_pre)
+            # calculate the test accuracy
+            test_accuracy = self.accuracy(test_images, test_labels) * 100
+            test_accuracy_plot.append(test_accuracy)
+            print(f"Test accuracy: {test_accuracy:.2f}%")
 
-        # Forward propagation (hidden to output)
-        output_pre = self.b_hidden_output + self.w_hidden_output @ hidden
-        output = self._sigmoid(output_pre)
+            # plot the loss and accuracy
+            if plot_stats:
+                plot(loss_plot, test_accuracy_plot, train_accuracy_plot)
 
+    def accuracy(self, images: NDArray, labels: NDArray) -> float:
+        """
+        Calculate the accuracy of the neural network
+        Args:
+            images (NDArray): The images to calculate the accuracy on
+            labels (NDArray): The labels of the images
+        Returns:
+            float: The accuracy of the neural network
+        """
+        output: NDArray = self._forward(images)
+        predicted_labels: NDArray = np.argmax(output, axis=1)
+        true_labels: NDArray = np.argmax(labels, axis=1)
+        accuracy: float = np.mean(predicted_labels == true_labels)
+        return accuracy
+
+    def predict(self, image: NDArray) -> int:
+        """
+        Predict the label of an image
+        Args:
+            image (NDArray): The image to predict
+        Returns:
+            int: The predicted label
+        """
+        output: NDArray = self._forward(image)
         return np.argmax(output)
 
 
@@ -79,10 +332,24 @@ def get_arguments():
     parser.add_argument(
         "-e",
         "--epochs",
-        action=EpochAction,
+        action=NonpositiveIntAction,
         type=int,
         default=3,
         help="Number of epochs to train the neural network",
+    )
+    parser.add_argument(
+        "-b",
+        "--batch-size",
+        action=NonpositiveIntAction,
+        type=int,
+        default=32,
+        help="Batch size for training the neural network",
+    )
+    parser.add_argument(
+        "-p",
+        "--plot-stats",
+        action="store_true",
+        help="Plot the loss and accuracy of the neural network",
     )
     parser.add_argument(
         "-l",
@@ -97,16 +364,8 @@ def get_arguments():
         "--learning-rate-decay",
         action=LearningRateAction,
         type=float,
-        default=1,
+        default=0.001,
         help="Learning rate decay for the neural network",
-    )
-    parser.add_argument(
-        "-le",
-        "--lr-decay-epoch",
-        action=EpochAction,
-        type=int,
-        default=1,
-        help="Epochs before the learning rate decays",
     )
     parser.add_argument(
         "-c",
@@ -124,39 +383,61 @@ def get_arguments():
     args = parser.parse_args()
     return (
         args.epochs,
+        args.batch_size,
+        args.plot_stats,
         args.learning_rate,
         args.learning_rate_decay,
-        args.lr_decay_epoch,
         args.custom_dataset,
         args.draw,
     )
 
 
 def main():
-    epochs, learning_rate, learning_rate_decay, lr_decay_epoch, custom_dataset, draw = (
-        get_arguments()
-    )
-    nn = NeuralNetwork(784, 20, 10)
+    (
+        epochs,
+        batch_size,
+        plot_stats,
+        learning_rate,
+        learning_rate_decay,
+        custom_dataset,
+        draw,
+    ) = get_arguments()
+    nn = NeuralNetwork(784, [512, 512], 10)
+
     if custom_dataset:
-        images, labels = DataLoader().load_custom_data()
+        train_images, train_labels, test_images, test_labels = DataLoader().load_custom_data()
     else:
-        images, labels = DataLoader().load_mnist_data()
-    nn.train(learning_rate, epochs, images, labels, learning_rate_decay, lr_decay_epoch)
+        train_images, train_labels, test_images, test_labels = DataLoader().load_mnist_data()
+
+    nn.train(
+        learning_rate=learning_rate,
+        epochs=epochs,
+        batch_size=batch_size,
+        train_images=train_images,
+        train_labels=train_labels,
+        test_images=test_images,
+        test_labels=test_labels,
+        plot_stats=plot_stats,
+        learning_rate_decay=learning_rate_decay,
+    )
+
     if draw and not custom_dataset:
         drawing_app = DrawingApp()
         print("Press 'R' to clear the window, 'ENTER' to continue and 'ESC' to exit")
         while drawing_app.running:
             image = drawing_app.get_image()
             if image is not None:
+                plt.clf()
                 plt.imshow(image.reshape(28, 28), cmap="gray")
                 plt.title(f"Prediction: {nn.predict(image)}")
                 plt.show()
     else:
         while True:
-            index = int(input(f"Enter an index[0 - {len(images) - 1}](-1 to exit): "))
+            index = int(input(f"Enter an index[0 - {len(test_images) - 1}](-1 to exit): "))
             if index == -1:
                 break
-            image = images[index]
+            image = test_images[index]
+            plt.clf()
             plt.imshow(image.reshape(28, 28), cmap="gray")
             plt.title(f"Prediction: {nn.predict(image)}")
             plt.show()
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import argparse
 import cv2
+import matplotlib.pyplot as plt
+from IPython import display
+from numpy.typing import NDArray
 
 
 class DataLoader:
@@ -14,7 +17,12 @@ class DataLoader:
         images = images.reshape(images.shape[0], (images.shape[1] * images.shape[2]))
         labels = np.eye(10)[labels]
 
-        return images, labels
+        # split the data into training and test sets
+        split = int(0.9 * images.shape[0])
+        train_images, test_images = images[:split], images[split:]
+        train_labels, test_labels = labels[:split], labels[split:]
+
+        return train_images, train_labels, test_images, test_labels
 
     @staticmethod
     def load_custom_data():
@@ -23,13 +31,18 @@ class DataLoader:
         images = np.array([np.array(image.split(" "), dtype=np.float32) for image in images]) / 255
         labels = np.eye(10)[labels]
 
-        return images, labels
+        # split the data into training and test sets
+        split = int(0.8 * images.shape[0])
+        train_images, test_images = images[:split], images[split:]
+        train_labels, test_labels = labels[:split], labels[split:]
+
+        return train_images, train_labels, test_images, test_labels
 
 
-class EpochAction(argparse.Action):
+class NonpositiveIntAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         if values < 1:
-            raise argparse.ArgumentError(self, "Epochs must be greater than 0")
+            raise argparse.ArgumentError(self, "Value should be greater than 0")
         setattr(namespace, self.dest, values)
 
 
@@ -60,12 +73,12 @@ class DrawingApp:
 
         elif event == cv2.EVENT_MOUSEMOVE:
             if self.drawing:
-                cv2.line(self.image, (self.x, self.y), (x, y), color=(255, 255, 255), thickness=40)
+                cv2.line(self.image, (self.x, self.y), (x, y), color=(255, 255, 255), thickness=35)
                 self.x, self.y = x, y
 
         elif event == cv2.EVENT_LBUTTONUP:
             self.drawing = False
-            cv2.line(self.image, (self.x, self.y), (x, y), color=(255, 255, 255), thickness=40)
+            cv2.line(self.image, (self.x, self.y), (x, y), color=(255, 255, 255), thickness=35)
 
     def get_image(self):
         cv2.namedWindow(self.window_name, cv2.WINDOW_AUTOSIZE)
@@ -88,3 +101,81 @@ class DrawingApp:
 
         self._reset()
         return image
+
+
+def plot(loss, test_accuracy, train_accuracy):
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.clf()
+
+    plt.subplot(1, 2, 1)
+    plt.plot(loss, label="Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.title("Loss over epochs")
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(test_accuracy, label="Test Accuracy")
+    plt.plot(train_accuracy, label="Train Accuracy")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy over epochs")
+    plt.legend()
+
+    plt.show(block=False)
+    plt.pause(0.1)
+
+
+class ImageAugmentation:
+    def __init__(self, image: NDArray):
+        self.image: NDArray = image.reshape(28, 28) * 255
+        self.h, self.w = self.image.shape
+
+    def __call__(self) -> NDArray:
+        random_zoom: float = np.random.uniform(1, 1.15)
+        self.image = self._zoom(random_zoom)
+        random_rotation: int = np.random.randint(-10, 10)
+        self.image = self._rotate(random_rotation)
+        random_shift_x: int = np.random.randint(-3, 3)
+        random_shift_y: int = np.random.randint(-3, 3)
+        self.image = self._shift(random_shift_x, random_shift_y)
+        random_noise: float = np.random.uniform(0, 20)
+        self.image = self._noise(random_noise)
+        return self.image.reshape(784) / 255
+
+    def _zoom(self, scale: float) -> NDArray:
+        h_new, w_new = int(self.h * scale), int(self.w * scale)
+        img_new: NDArray = cv2.resize(self.image, (w_new, h_new))
+
+        if h_new < self.h:
+            pad_top = (self.h - h_new) // 2
+            pad_bottom = self.h - h_new - pad_top
+            img_new = np.pad(img_new, ((pad_top, pad_bottom), (0, 0)), mode="constant")
+        elif h_new > self.h:
+            img_new = img_new[(h_new - self.h) // 2 : (h_new + self.h) // 2, :]
+
+        if w_new < self.w:
+            pad_left = (self.w - w_new) // 2
+            pad_right = self.w - w_new - pad_left
+            img_new = np.pad(img_new, ((0, 0), (pad_left, pad_right)), mode="constant")
+        elif w_new > self.w:
+            img_new = img_new[:, (w_new - self.w) // 2 : (w_new + self.w) // 2]
+
+        return img_new
+
+    def _rotate(self, angle: float) -> NDArray:
+        M: NDArray = cv2.getRotationMatrix2D(((self.w - 1) / 2, (self.h - 1) / 2), angle, 1)
+        img_new: NDArray = cv2.warpAffine(self.image, M, (self.w, self.h))
+        return img_new
+
+    def _shift(self, dx: int, dy: int) -> NDArray:
+        M: NDArray = np.float32([[1, 0, dx], [0, 1, dy]])
+        img_new: NDArray = cv2.warpAffine(self.image, M, (self.w, self.h))
+        return img_new
+
+    def _noise(self, sigma: float) -> NDArray:
+        noise: NDArray = np.random.normal(0, sigma, self.image.shape)
+        img_new: NDArray = self.image + noise
+        img_new = np.clip(img_new, 0, 255)
+        return img_new
